64a9939f3345d37925b03d8a4aaf38b07d8ed541
Batchunag Dashdemberel <gtokyo2010@gmail.com>
2020-09-21 21:19:44 +0900

    MVP for autocomplete.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Autocomplete from "./Autocomplete";
 
+var words_w = ["ᠠᠪᠤ", "ᠠᠭᠠᠭ", "ᠠᠭᠠᠭᠢᠮ᠎ᠠ"];
+var words_l = ["abu", "agag", "agagim"];
+var words_c = ["aав", "ааг", "аагим"];
+
+
+function handleChange(e) {
+  console.log(e.target.value);
+}
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        {/* <input type='text' onChange={handleChange}/> */}
+        <Autocomplete
+        words_w={words_w}
+        words_l={words_l}
+        // options={[
+        //   "Papaya",
+        //   "Persimmon",
+        //   "Paw Paw",
+        //   "Prickly Pear",
+        //   "Peach",
+        //   "Pomegranate",
+        //   "Pineapple"
+        // ]}
+      />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
